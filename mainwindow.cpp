a96b7f957799b2ec224e2c801c3e6a439ff55607
Wzing0421 <wzing0421@163.com>
2019-07-25 22:44:45 +0800

    big endean

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,6 +20,10 @@ MainWindow::MainWindow(QWidget *parent) :
         connect(recvSocket,SIGNAL(readyRead()),this,SLOT(recvInfo()));
     }
 
+    init_auth();
+    init_Rsp();
+    init_DeRegisterReq();
+    init_DeRegisterRsp();
 }
 
 MainWindow::~MainWindow()
@@ -38,20 +42,74 @@ void MainWindow::recvInfo(){
         QHostAddress UEaddr;
         UEaddr.setAddress("162.105.85.198");
         quint16 UEport = 10002;
-        if(datagram[0] == 0x01){//是注册的,需要判别是否是带有鉴权注册的
-            if(datagram.size()==21){//不带鉴权的注册
-                datagram[2] = 0x02;
+        if(datagram[10] == 0x01){//是注册的,需要判别是否是带有鉴权注册的
+            if(datagram.size()==29){//不带鉴权的注册
                 qDebug()<<"接收到的数据长度 "<<datagram.size();
-                int num = recvSocket->writeDatagram(datagram,UEaddr,UEport);
+                int num = recvSocket->writeDatagram((char*)authCommand,sizeof(authCommand),UEaddr,UEport);
                 qDebug()<<"authorized command size: "<<num;
             }
             else{//带有鉴权的注册
-                datagram[2] = 0x03;
                 qDebug()<<"接收到的数据长度 "<<datagram.size();
-                int num = recvSocket->writeDatagram(datagram,UEaddr,UEport);
-                qDebug()<<"voice register rsp size: "<<num;
+                int num = recvSocket->writeDatagram((char*)voiceRegisterRsp,sizeof(voiceRegisterRsp),UEaddr,UEport);
+                qDebug()<<"发送voice register rsp size: "<<num;
             }
+        }
+        else if(datagram[10] == 0x04){
+            qDebug()<<"接收到voice DeRegister Req ,长度 "<<datagram.size();
+            int num = recvSocket->writeDatagram((char*)voiceDeRegisterRsp,sizeof(voiceRegisterRsp),UEaddr,UEport);
+            qDebug()<<"voice DeRegister rsp size: "<<num;
+        }
+        else if(datagram[10] == 0x05){
+            qDebug()<<"接收到voice DeRegister Rsp ,长度 "<<datagram.size();
         }
 
     }
+}
+
+void MainWindow::init_auth(){//初始化鉴权信息
+
+    memset(authCommand,0,sizeof(authCommand));
+    authCommand[0] = 0x00;//Protocal version
+    authCommand[1] = 0x10;//Message length == 16 byte
+    authCommand[2] = 0x02;//Message type
+    //后面的STMSI和Nonce我先都置成0
+    authCommand[15] = 0x01;//算了我把nonce最后一位置为1吧
+
+}
+
+void MainWindow::init_Rsp(){//初始化voice register Rsp
+    memset(voiceRegisterRsp,0,sizeof(voiceRegisterRsp));
+    voiceRegisterRsp[0] = 0x00;//Protocol version
+    voiceRegisterRsp[1] = 0x0a;//Message length == 10 byte
+    voiceRegisterRsp[2] = 0x03;//Message type
+    //STMSI 先设置成0
+    voiceRegisterRsp[8] = 0x00;//casue : 成功
+    voiceRegisterRsp[9] = 0x01;//单位是3600s，置为0x01表示每3600s周期注册一次
+}
+
+void MainWindow::init_DeRegisterReq(){//初始化voice DeRegister Req
+    voiceDeRegisterReq[0] = 0x00;
+    voiceDeRegisterReq[1] = 0x0a;//message length == 10byte
+    voiceDeRegisterReq[2] = 0x04;//message type;
+    //略去STMSI部分
+    voiceDeRegisterReq[8] = 0x00;//网络侧注销
+    voiceDeRegisterReq[9] = 0x01;
+}
+
+void MainWindow::init_DeRegisterRsp(){
+    memset(voiceDeRegisterRsp,0,sizeof(voiceDeRegisterRsp));
+    voiceDeRegisterRsp[0] = 0x00;
+    voiceDeRegisterRsp[1] = 0x08;//message length
+    voiceDeRegisterRsp[2] = 0x05;//message type
+    //后面5个字节的STMSI目前就是0，是和前面RegMsg是一样的
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QHostAddress UEaddr;
+    UEaddr.setAddress("162.105.85.198");
+    quint16 UEport = 10002;
+    int num = recvSocket->writeDatagram((char*)voiceDeRegisterReq,sizeof(voiceDeRegisterReq),UEaddr,UEport);
+    qDebug()<<"voice DeRegister Req size: "<<num;
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -23,16 +23,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     QUdpSocket *recvSocket;
-    //QUdpSocket *sendSocket;//用于发送注册信息的socket
+
     quint16 recvPort;//接收注册信息用的端口
-    //quint16 regsendPort;//向PCC发送注册信息的目的端口
 
+    unsigned char authCommand[16]; //我先暂定是16字节，其中Nonce是8字节，前面信息占8字节
+    unsigned char voiceRegisterRsp[8];//这是voice Register Rsp, 一期不携带IMPU(用户IMS标识)
+    unsigned char voiceDeRegisterReq[10];//表示的是PCC端发送的voice DeRegister Req
+    unsigned char voiceDeRegisterRsp[8];
 
+    void init_auth();
+    void init_Rsp();
+    void init_DeRegisterReq();
+    void init_DeRegisterRsp();
 private:
     Ui::MainWindow *ui;
 
 private slots:
     void recvInfo();//接收注册消息的回调函数
+    void on_pushButton_clicked();
 };
 
 #endif // MAINWINDOW_H
